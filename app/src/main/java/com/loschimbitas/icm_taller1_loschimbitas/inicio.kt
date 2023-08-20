6070fdd2f5da3b0490b23de5d0954f51b3807430
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-08-20 17:31:39 -0500

    arreglo de nombres de archivos

--- app/src/main/java/com/loschimbitas/icm_taller1_loschimbitas/inicio.kt
+++ app/src/main/java/com/loschimbitas/icm_taller1_loschimbitas/inicio.kt
@@ -3,9 +3,9 @@ package com.loschimbitas.icm_taller1_loschimbitas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.inicio)
     }
 }
