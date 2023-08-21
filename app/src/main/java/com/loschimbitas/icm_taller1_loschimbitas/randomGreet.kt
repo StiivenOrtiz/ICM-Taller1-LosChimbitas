e710b916093cbdf5e1272cb6a7d0d5e3ae5086c4
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-08-21 16:05:40 -0500

    Creación pantalla: Random Greet

---
+++ app/src/main/java/com/loschimbitas/icm_taller1_loschimbitas/randomGreet.kt
@@ -0,0 +1,11 @@
+package com.loschimbitas.icm_taller1_loschimbitas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class randomGreet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.random_greet)
+    }
+}
